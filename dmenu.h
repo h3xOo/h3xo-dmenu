191d0ef002f6b5f19b9cdc420fceed180c3fe77f
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2023-02-09 13:18:23 +0100

    Everywhere: clang-format

--- dmenu.h
+++ dmenu.h
@@ -9,19 +9,27 @@
 #include "drw.h"
 
 /* macros */
-#define INTERSECT(x, y, w, h, r)                                               \
-    (MAX(0, MIN((x) + (w), (r).x_org + (r).width) - MAX((x), (r).x_org)) &&    \
-     MAX(0, MIN((y) + (h), (r).y_org + (r).height) - MAX((y), (r).y_org)))
+#define INTERSECT(x, y, w, h, r)                                              \
+    (MAX (0, MIN ((x) + (w), (r).x_org + (r).width) - MAX ((x), (r).x_org))   \
+     && MAX (0,                                                               \
+             MIN ((y) + (h), (r).y_org + (r).height) - MAX ((y), (r).y_org)))
 #define LENGTH(X) (sizeof X / sizeof X[0])
-#define TEXTW(X) (drw_fontset_getwidth(drw, (X)) + lrpad)
+#define TEXTW(X) (drw_fontset_getwidth (drw, (X)) + lrpad)
 
 /* define opaqueness */
 #define OPAQUE 0xFFU
 
 /* enums */
-enum { SchemeNorm, SchemeSel, SchemeOut, SchemeLast }; /* color schemes */
+enum
+{
+    SchemeNorm,
+    SchemeSel,
+    SchemeOut,
+    SchemeLast
+}; /* color schemes */
 
-struct item {
+struct item
+{
     char *text;
     struct item *left, *right;
     int out;
@@ -51,9 +59,9 @@ static int depth;
 static Colormap cmap;
 static Clr *scheme[SchemeLast];
 
-static int (*fstrncmp)(const char *, const char *, size_t) = strncmp;
-static char *(*fstrstr)(const char *, const char *) = strstr;
+static int (*fstrncmp) (const char *, const char *, size_t) = strncmp;
+static char *(*fstrstr) (const char *, const char *) = strstr;
 
-static unsigned int textw_clamp(const char *str, unsigned int n);
+static unsigned int textw_clamp (const char *str, unsigned int n);
 
 #endif /* DMENU_H__ */
