8c654c5db3fd7f7d848613351b9e56769829b1e1
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2024-06-16 20:22:33 +0200

    Clang-format

--- config.h
+++ config.h
@@ -6,21 +6,15 @@
 
 static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom     */
 static int centered = 1; /* -c option; centers dmenu on screen */
-static const int min_width = 500; /* minimum width when centered */
+static int const min_width = 500; /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char* fonts[] = {
-    "SauceCodePro Nerd Font:size=12",
-    "JoyPixels:pixelsize=12:antialias=true:autohint=true"
-};
-static const char* prompt = NULL; /* -p  option; prompt to the left of input field */
-static const char* colors[SchemeLast][2] = {
+static char const *fonts[] = { "SauceCodePro Nerd Font:size=12", "JoyPixels:pixelsize=12:antialias=true:autohint=true" };
+static char const *prompt = NULL; /* -p  option; prompt to the left of input field */
+static char const *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm] = { "#BBBBBB", "#222222" },
-    [SchemeSel] = { "#EEEEEE", "#AA758C" },
-	[SchemeSelHighlight] = { "#ffc978", "#005577" },
-	[SchemeNormHighlight] = { "#ffc978", "#222222" },
-    [SchemeOut] = { "#000000", "#00FFFF" },
-	[SchemeCursor] = { "#222222", "#bbbbbb"},
+        [SchemeNorm] = { "#BBBBBB", "#222222" },         [SchemeSel] = { "#EEEEEE", "#AA758C" },
+        [SchemeSelHighlight] = { "#ffc978", "#005577" }, [SchemeNormHighlight] = { "#ffc978", "#222222" },
+        [SchemeOut] = { "#000000", "#00FFFF" },          [SchemeCursor] = { "#222222", "#bbbbbb" },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines
@@ -31,18 +25,18 @@ static unsigned int lines = 15;
  * Characters not considered part of a word while deleting words
  * for example: " /?\"&[]"
  */
-static const char worddelimiters[] = " ";
+static char const worddelimiters[] = " ";
 
 /*
  * -vi option; if nonzero, vi mode is always enabled and can be
  * accessed with the global_esc keysym + mod mask
  */
 static unsigned int vi_mode = 1;
-static const unsigned int start_mode = 1;			/* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
-static Key global_esc = { XK_Escape, 0 };	/* escape key when vi mode is not enabled explicitly */
-static const Key quit_keys[] = {
-	/* keysym	modifier */
-	{ XK_q,		0 }
+static unsigned int const start_mode = 1;   /* mode to use when -vi is passed. 0 = insert mode, 1 = normal mode */
+static Key global_esc = { XK_Escape, 0 }; /* escape key when vi mode is not enabled explicitly */
+static Key const quit_keys[] = {
+ /* keysym	modifier */
+        { XK_q, 0 }
 };
 
 /* Size of the window border */
