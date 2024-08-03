a987d4f4c2810500bc109cfabf1ac4e8b604fcdb
Stanisław Wiśniewski <s.wisniewski1101@gmail.com>
2024-08-03 08:23:40 +0200

    Apply upstream patches

--- drw.c
+++ drw.c
@@ -8,51 +8,38 @@
 #include <string.h>
 
 #define UTF_INVALID 0xFFFD
-#define UTF_SIZ     4
 
-static unsigned char const utfbyte[UTF_SIZ + 1] = { 0x80, 0, 0xC0, 0xE0, 0xF0 };
-static unsigned char const utfmask[UTF_SIZ + 1] = { 0xC0, 0x80, 0xE0, 0xF0, 0xF8 };
-static long const utfmin[UTF_SIZ + 1] = { 0, 0, 0x80, 0x800, 0x10000 };
-static long const utfmax[UTF_SIZ + 1] = { 0x10FFFF, 0x7F, 0x7FF, 0xFFFF, 0x10FFFF };
-
-static long utf8decodebyte(char const c, size_t *i)
+static int utf8decode(char const *s_in, long *u, int *err)
 {
-        for (*i = 0; *i < (UTF_SIZ + 1); ++(*i))
-                if (((unsigned char)c & utfmask[*i]) == utfbyte[*i])
-                        return (unsigned char)c & ~utfmask[*i];
-        return 0;
-}
+        static unsigned char const lens[] = {
+                /* 0XXXX */ 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+                /* 10XXX */ 0, 0, 0, 0, 0, 0, 0, 0,  /* invalid */
+                /* 110XX */ 2, 2, 2, 2,
+                /* 1110X */ 3, 3,
+                /* 11110 */ 4,
+                /* 11111 */ 0,  /* invalid */
+        };
+        static unsigned char const leading_mask[] = { 0x7F, 0x1F, 0x0F, 0x07 };
+        static unsigned int const overlong[] = { 0x0, 0x80, 0x0800, 0x10000 };
 
-static size_t utf8validate(long *u, size_t i)
-{
-        if (!BETWEEN(*u, utfmin[i], utfmax[i]) || BETWEEN(*u, 0xD800, 0xDFFF))
-                *u = UTF_INVALID;
-        for (i = 1; *u > utfmax[i]; ++i)
-                ;
-        return i;
-}
-
-static size_t utf8decode(char const *c, long *u, size_t clen)
-{
-        size_t i, j, len, type;
-        long udecoded;
-
+        unsigned char const *s = (unsigned char const *)s_in;
+        int len = lens[*s >> 3];
         *u = UTF_INVALID;
-        if (!clen)
-                return 0;
-        udecoded = utf8decodebyte(c[0], &len);
-        if (!BETWEEN(len, 1, UTF_SIZ))
+        *err = 1;
+        if (len == 0)
                 return 1;
-        for (i = 1, j = 1; i < clen && j < len; ++i, ++j) {
-                udecoded = (udecoded << 6) | utf8decodebyte(c[i], &type);
-                if (type)
-                        return j;
-        }
-        if (j < len)
-                return 0;
-        *u = udecoded;
-        utf8validate(u, len);
 
+        long cp = s[0] & leading_mask[len - 1];
+        for (int i = 1; i < len; i++) {
+                if (s[i] == '\0' || (s[i] & 0xc0) != 0x80)
+                        return i;
+                cp = (cp << 6) | (s[i] & 0x3f);
+        }
+        /* out of range, surrogate, overlong encoding */
+        if (cp > 0x10ffff || (cp >> 11) == 0x1b || cp < overlong[len - 1])
+                return len;
+        *err = 0;
+        *u = cp;
         return len;
 }
 
@@ -223,7 +210,7 @@ int drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned in
         unsigned int tmpw, ew, ellipsis_w = 0, ellipsis_len, hash, h0, h1;
         XftDraw *d = NULL;
         Fnt *usedfont, *curfont, *nextfont;
-        int utf8strlen, utf8charlen, render = x || y || w || h;
+        int utf8strlen, utf8charlen, utf8err, render = x || y || w || h;
         long utf8codepoint = 0;
         char const *utf8str;
         FcCharSet *fccharset;
@@ -232,7 +219,8 @@ int drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned in
         XftResult result;
         int charexists = 0, overflow = 0;
     /* keep track of couple codepoints for which we have no match. */
-        static unsigned int nomatches[128], ellipsis_width;
+        static unsigned int nomatches[128], ellipsis_width, invalid_width;
+        static char const invalid[] = "�";
 
         if (!drw || (render && (!drw->scheme || !w)) || !text || !drw->fonts)
                 return 0;
@@ -250,19 +238,21 @@ int drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned in
         usedfont = drw->fonts;
         if (!ellipsis_width && render)
                 ellipsis_width = drw_fontset_getwidth(drw, "...");
+        if (!invalid_width && render)
+                invalid_width = drw_fontset_getwidth(drw, invalid);
         while (1) {
-                ew = ellipsis_len = utf8strlen = 0;
+                ew = ellipsis_len = utf8err = utf8charlen = utf8strlen = 0;
                 utf8str = text;
                 nextfont = NULL;
                 while (*text) {
-                        utf8charlen = utf8decode(text, &utf8codepoint, UTF_SIZ);
+                        utf8charlen = utf8decode(text, &utf8codepoint, &utf8err);
                         for (curfont = drw->fonts; curfont; curfont = curfont->next) {
                                 charexists = charexists || XftCharExists(drw->dpy, curfont->xfont, utf8codepoint);
                                 if (charexists) {
                                         drw_font_getexts(curfont, text, utf8charlen, &tmpw, NULL);
                                         if (ew + ellipsis_width <= w) {
-                        /* keep track where the ellipsis
-                         * still fits */
+                                                /* keep track where the ellipsis
+                                                 * still fits */
                                                 ellipsis_x = x + ew;
                                                 ellipsis_w = w - ew;
                                                 ellipsis_len = utf8strlen;
@@ -270,19 +260,19 @@ int drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned in
 
                                         if (ew + tmpw > w) {
                                                 overflow = 1;
-                        /* called from
-                         * drw_fontset_getwidth_clamp(): it
-                         * wants the width AFTER the
-                         * oferflow
-                         */
+                                                /* called from
+                                                 * drw_fontset_getwidth_clamp(): it
+                                                 * wants the width AFTER the
+                                                 * oferflow
+                                                 */
                                                 if (!render)
                                                         x += tmpw;
                                                 else
                                                         utf8strlen = ellipsis_len;
                                         } else if (curfont == usedfont) {
-                                                utf8strlen += utf8charlen;
                                                 text += utf8charlen;
-                                                ew += tmpw;
+                                                utf8strlen += utf8err ? 0 : utf8charlen;
+                                                ew += utf8err ? 0 : tmpw;
                                         } else {
                                                 nextfont = curfont;
                                         }
@@ -290,7 +280,7 @@ int drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned in
                                 }
                         }
 
-                        if (overflow || !charexists || nextfont)
+                        if (overflow || !charexists || nextfont || utf8err)
                                 break;
                         else
                                 charexists = 0;
@@ -304,6 +294,12 @@ int drw_text(Drw *drw, int x, int y, unsigned int w, unsigned int h, unsigned in
                         }
                         x += ew;
                         w -= ew;
+                }
+                if (utf8err && (!render || invalid_width < w)) {
+                        if (render)
+                                drw_text(drw, x, y, w, h, 0, invalid, invert);
+                        x += invalid_width;
+                        w -= invalid_width;
                 }
                 if (render && overflow)
                         drw_text(drw, ellipsis_x, y, ellipsis_w, h, 0, "...", invert);
